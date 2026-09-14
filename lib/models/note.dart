931ce70faeabc9b6e0a4666c8ac659a09fef9449
Huckleberry-Jon <Huckleberryservices@yahoo.com>
2026-09-13 20:49:48 -0500

    Add note serialization for local persistence

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -28,4 +28,24 @@ class Note {
       updatedAt: updatedAt ?? this.updatedAt,
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+    };
+  }
+
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      content: json['content'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
+    );
+  }
 }
